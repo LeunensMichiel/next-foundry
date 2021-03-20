86cdf95ed4790b26a91b32fcfaf4c79c51791f62
LeunensMichiel <michiel.leunens@gmail.com>
2021-03-20 15:56:28 +0100

    feat: Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-typescript', 'prettier'],
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css';
 
 import { AppProps } from 'next/app';
 
+// TODO: Fix this error below and add prettierconfig
+
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
