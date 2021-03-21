c080b616bb1d222fc1efa078764aff2b41d1f25d
LeunensMichiel <michiel.leunens@gmail.com>
2021-03-21 11:43:19 +0100

    chore: fix issues with ES-Lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import '../styles/globals.css';
 
 import { AppProps } from 'next/app';
 
-// TODO: Fix this error below and add prettierconfig
-
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
